1a4078d0b326e33022de7159e02aec8f6ffb4d20
LucaSab48 <lucasabelli@hotmail.com>
2023-09-14 14:18:22 -0400

    Lecutre 2: Draw an alien

--- Projects/02-draw-an-alien/js/script.js
+++ Projects/02-draw-an-alien/js/script.js
@@ -19,19 +19,10 @@ function preload() {
 /**
  * Description of setup
 */
-// this is a comment
 function setup() {
 
-  createCanvas(500, 500);
-  background(0, 255, 0);
-  stroke(255, 0, 255, 50);
-  strokeWeight (10);
-  fill(255, 50, 100);
-  ellipse(250, 250, 100);
-  fill(255, 0, 255);
-  rect(100, 200, 50, 50);
-
 }
+
 
 /**
  * Description of draw()
