96507ee1b304838863c054b4d5082d88767a14d2
LucaSab48 <lucasabelli@hotmail.com>
2023-10-04 14:57:08 -0400

    A5: continued

--- Projects/05-looking-for-love/js/script.js
+++ Projects/05-looking-for-love/js/script.js
@@ -6,65 +6,127 @@
  * and this description to match your project!
  */
 
-"use strict";
-
-/**
- * Description of preload
-*/
 function preload() {
 
 }
 
-let staticAmount = 1000;
+let state = "title";
 
-let user = {
-    size: 100,
-    fill: 255
-}
-
-let covid19 = {
-    x: 0,
-    y: 250,
+let circle1 = {
+    x: 640,
+    y: 640,
     size: 100,
     vx: 0,
     vy: 0,
-    speed: 5,
-    fill: {
-        r: 255,
-        g: 0,
-        b: 0
-    }
+    speed: 5
 };
+
+let circle2 = {
+    x: 1280,
+    y: 640,
+    size: 100,
+    vx: 0,
+    vy: 0,
+    speed: 5
+};
+
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    covid19.y = random(0, height);
-    covid19.vx = covid19.speed;
+    circle1.vx = random(-circle1.speed, circle1.speed);
+    circle1.vy = random(-circle1.speed, circle1.speed);
+    circle2.vx = random(-circle2.speed, circle2.speed);
+    circle2.vy = random(-circle2.speed, circle2.speed);
+   
 }
+
 
 function draw() {
     background(0);
-    for (let i = 0; i < staticAmount; i++) {
-       let posX = random(0, width);
-       let posY = random(0, height);
-       stroke(255);
-       point(posX, posY);
+    if (state === "title") {
+        title();
     }
-    let userX = mouseX;
-    let userY = mouseY;
-    fill(user.fill);
-    ellipse(userX, userY, user.size);
-    covid19.x += covid19.vx;
-    covid19.y += covid19.vy;
-    fill(covid19.fill.r, covid19.fill.g, covid19.fill.b);
-    noStroke();
-    ellipse(covid19.x, covid19.y, covid19.size);
-    if (covid19.x > width) {
-        covid19.x = 0;
-        covid19.y = random(0, height);
+    else if (state === "simulation") {
+        simulation();
     }
-    let d = dist(userX, userY, covid19.x, covid19.y);
-    if (d < covid19.size / 2 + user.size / 2) {
-        noLoop();
+    else if (state === "love") {
+        love();
+    }
+    else if (state === "sad") {
+        sad();
     }
 }
+
+
+function move() {
+    circle1.x += circle1.vx;
+    circle1.y += circle1.vy;
+    circle2.x += circle2.vx;
+    circle2.y += circle2.vy; 
+}
+
+
+function checkOffScreen() {
+    if(circle1.x < 0 || circle1.x > width || circle1.y < 0 || circle1.y > height) {
+        state = "sad";
+    }
+    if(circle2.x < 0 || circle2.x > width || circle2.y < 0 || circle2.y > height) {
+        state = "sad";
+    }
+}
+
+
+function display() {
+    fill(255);
+    ellipse(circle1.x, circle1.y, circle1.size);
+    ellipse(circle2.x, circle2.y, circle2.size);
+}
+
+
+function checkOverlap() {
+    let d = dist(circle1.x, circle1.y, circle2.x, circle2.y);
+    if(d < circle1.size / 2 + circle2.size / 2) {
+        state = "love";
+    }
+}
+
+
+function simulation() {
+    move();
+    display();
+    checkOffScreen();
+    checkOverlap();
+}
+
+
+function title() {
+    textAlign(CENTER, CENTER);
+    textSize(50);
+    fill(255);
+    text("does true love exist?", width/2, height/2);
+}
+
+
+function love() {
+    textAlign(CENTER, CENTER);
+    textSize(70);
+    fill(255);
+    text("LOVE DOES EXIST!", width/2, height/2);
+}
+
+
+function sad() {
+    textAlign(CENTER, CENTER);
+    textSize(50);
+    fill(255);
+    text("i guess we all die alone", width/2, height/2);
+}
+
+
+function mousePressed() {
+    if (state === "title") {
+        state = "simulation";
+    }
+}
+
+
